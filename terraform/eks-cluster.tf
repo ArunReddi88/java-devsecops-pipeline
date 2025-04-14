f2a0fe839b5f98ca814a5e699c8a0d72ef4197a7
ArunReddi88 <arunkrad608@gmail.com>
2025-04-14 18:12:33 +0530

    Create eks-cluster.tf

---
+++ terraform/eks-cluster.tf
@@ -0,0 +1,28 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 20.0"
+  cluster_name    = "${var.project_name}-eks"
+  cluster_version = "1.29"
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  enable_irsa = true
+
+  eks_managed_node_group_defaults = {
+    instance_types = ["t3.medium"]
+  }
+
+  eks_managed_node_groups = {
+    default = {
+      desired_capacity = 2
+      min_capacity     = 1
+      max_capacity     = 3
+    }
+  }
+
+  tags = {
+    Environment = "production"
+    Terraform   = "true"
+  }
+}
